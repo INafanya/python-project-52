74405ce240309bce12f722f04d488a0449d269ed
Andrey Solovyov <solovyov.net@gmail.com>
2025-10-21 00:02:20 +0300

    Fix errors

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -109,7 +109,8 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 }
 
 
