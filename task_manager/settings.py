290aa82110ba7f451caf8af97c97f74a673d75a6
Andrey Solovyov <solovyov.net@gmail.com>
2025-10-21 19:03:17 +0300

    Fix settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'cg#p$g+j9tax!#t2_+&k3q+pmusj6yjpkag')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool(os.getenv('DJANGO_DEBUG', True))
+DEBUG = bool(os.getenv('DJANGO_DEBUG', False))
 
 # ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'webserver').split()
